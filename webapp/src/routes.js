28898d6857bf4b83e012ef34b9b3373ebfe17bc6
Maison GUILLEMAUT <servi@NASA.maison.guillemaut.net>
2019-11-11 19:44:25 +0100

    lastlastcommit

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import App from './App';
-import App2 from './pages/2ndPage';
 
 const Routes = () => (
     <Router>
             <Route exact path="/" component={App} />
-            <Route path="/App2" component={App2} />
     </Router>
 );
 
